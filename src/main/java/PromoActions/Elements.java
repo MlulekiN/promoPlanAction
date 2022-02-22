0601a8fb6dab04f48149f748e9b5e30db3416d6e
Mlulekigit config --global user.email mlu78nkosi@gmail.com <mlu78nkosi@gmail.com>
2022-02-22 11:51:14 +0200

    Edited and updated with new lines of code.

--- src/main/java/PromoActions/Elements.java
+++ src/main/java/PromoActions/Elements.java
@@ -11,7 +11,7 @@ public class Elements {
     public static By customerCode = By.xpath("//div[@data-qtip='Customer code']/descendant::div[@data-ref='triggerEl']");
     public static By inputID = By.xpath("//div[@sm1-id='likeValue']/descendant::div[contains(@name,'sm1textbox')]");
     public static By okButtonElement = By.xpath("//div[@sm1-id='LOGICALTEXTBOXCOLUMNPOPUP']//span[@sm1-id='SM1OkButton']/child::span[@data-ref='btnWrap']");
-    public static By selectChosenID = By.xpath("//tr[@class='sm1-row-no-error  x-grid-row']/child::td/div[text()='EC94791']");
+    public static String selectChosenID = "//tr[@class='sm1-row-no-error  x-grid-row']/child::td/div[text()='EC94791']";
     public static By okButton2 = By.xpath("//div[@sm1-id='LOGICALAVANCEDENTITYSELECTORPOPUP']/descendant::span[text()='OK']");
     public static By innerOkButton = By.xpath("//span[contains(@id,'sm1okbutton')]/descendant::span[@data-ref='btnEl']");
     public static By sellInDate = By.xpath("//div[@sm1-id='DATE_SELLIN']/descendant::div[contains(@id,'sm1dateperiod')]/child::input[@value='Start Date']");
@@ -23,11 +23,16 @@ public class Elements {
     public static By sellOutDate = By.xpath("//div[@sm1-id='DATE_SELLOUT']/descendant::div[contains(@id,'sm1dateperiod')]/input");
     public static By sellOutStart = By.xpath("//td[contains(@class,'x-datepicker-cell') and (@aria-label='March 04') and not (@data-qtip='Date Before Min Date')]");
     public static By sellOutEnd = By.xpath("//td[(@aria-label='March 09') and (@data-qtip='Date After Max Date')]/preceding-sibling::td[@aria-label='March 08']");
-    public static By okButtonSellOut = By.xpath("//span[@data-ref='btnIconEl']/following-sibling::span[text()='OK']");
+    public static By okButtonSellOut = By.xpath("//div[@id='sm1WaitDiv']/following-sibling::div[contains(@class,'daterangepicker')]/following-sibling::div[contains(@class,'daterangepicker')]/descendant::span[text()='OK']");
+    public static By generalInfo = By.xpath("//span[text()='General info']");
+    public static By tprRateIn = By.xpath("//div[text()='TPR in Rate ']/ancestor::td[contains(@class,'x-grid-cell x-grid-td x-grid-cell-sm1gridhtmlcolumn')]/preceding-sibling::td");
     public static By productCreate = By.xpath("//div[@sm1-id='TAB_H5_PRODUCTS']/descendant::span[text()='Products']");
-    public static By addButtonCreateProd = By.xpath("//span[@sm1-id='AddButton']");
+    public static By addButtonCreateProd = By.xpath("//div[@class='x-panel sm1-tabitem x-fit-item x-panel-default']/descendant::span[@sm1-id='AddButton']");
     public static By customerAdd = By.xpath("//div[text()='BACCO PINOT 0.75 lt']");
     public static By okButtonToAddCust = By.xpath("//span[contains(@id,'sm1okbutton')]/span[text()='OK']");
-    public static By cockPit = By.xpath("//span[text()='Cockpit']");
+    public static By saveButton = By.xpath("//span[text()='Save']");
+    public static By cockPit = By.xpath("//span[text()='Cockpit']/parent::span");
+    public static By promoWhatIf = By.xpath("//a[@sm1-id='ACTION_ACT_H5_PROMOSHOTSPOPUP']");
+    public static By promoOkButton = By.xpath("//div[contains(@class,'x-window sm1-popup')]/descendant::span[text()='OK']");
 
 }
--- src/main/java/PromoActions/WhatIfScenario.java
+++ src/main/java/PromoActions/WhatIfScenario.java
@@ -3,15 +3,12 @@ package PromoActions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 
 public class WhatIfScenario {
 
@@ -27,74 +24,69 @@ public class WhatIfScenario {
     @Test
     public void tpmWhatIfScenario() {
 
-        String customer_code = "EC12345";
+        String selectChosenID = "EC94791";
 
-        String url = "https://to-pdi.salesperformanceplatform.com/xtelsp-automhfx/web/SM1V6/SM1.aspx?DBG=1";
-        //String url = "https://to-pdi.salesperformanceplatform.com/xtelsp-autom/web/?DBG=1";
+        //  String url = "https://to-pdi.salesperformanceplatform.com/xtelsp-automhfx/web/SM1V6/SM1.aspx?DBG=1";
+        String url = "https://to-pdi.salesperformanceplatform.com/xtelsp-autom/web/?DBG=1";
         driver.get(url);
         driver.manage().window().maximize();
         WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(300));
 
-//        step 1, user opens the application - > expected: the application is open
         WebElement homeToolBar = wait1.until(ExpectedConditions.presenceOfElementLocated(Elements.homeToolBar));
         homeToolBar.click();
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 
-        // step 2, the user select the relevant tab to create promo - > expected: various promotions displayed
         WebElement promotionalActions = driver.findElement(Elements.promotionalActions);
         promotionalActions.click();
 
-// step 3, user clicks add button - > expected: the add button leads to multiple levels and promotions
         WebElement addButton = wait.until(ExpectedConditions.visibilityOfElementLocated(Elements.addButton));
         addButton.click();
 
-// step 4, the user selects new promotion to create - > expected: the appropriate promo to be chosen
         WebElement customerInput = wait.until(ExpectedConditions.elementToBeClickable(Elements.customerInput));
         customerInput.click();
 
-        // step 5, the user selects customer level - > expected: level 3 which is the desired level to be selected
         WebElement levelChoose = wait.until(ExpectedConditions.elementToBeClickable(Elements.levelChoose));
         levelChoose.click();
-//
-////        step 6, the user clicks on trigger to input the customer code - > expected: the trigger leads to the search option
-//        WebElement customerCode = wait.until(ExpectedConditions.elementToBeClickable(Elements.customerCode));
-//        customerCode.click();
-//
-//        // step 7, input the customer code on search bar - > expected: the code to be populated on the 'like' bar
-//        WebElement inputID = wait.until(ExpectedConditions.visibilityOfElementLocated(Elements.inputID));
-//        inputID.sendKeys("EC94791");
-//
-//        // step 8 , select the ok button element - > expected: the ok button to be selected to displayed appropriate customer
-//        WebElement okButtonElement = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.okButtonElement));
-//        okButtonElement.click();
+        {
+            new WebDriverWait(driver, Duration.ofSeconds(5))
+                    .ignoring(StaleElementReferenceException.class)
+                    .until((WebDriver d) -> {
+                        d.findElement(Elements.customerCode).click();
+                        return true;
+                    });
+        }
 
-        // step 9, confirmation of customer id, level, and name - >expected the display of all customer details
-        WebElement selectChosenID = wait.until(ExpectedConditions.elementToBeClickable(Elements.selectChosenID));
-        selectChosenID.click();
+        WebElement inputID = wait.until(ExpectedConditions.visibilityOfElementLocated(Elements.inputID));
+        inputID.sendKeys("EC94791");
 
-        // Step 10, select the ok button - > expected: the ok button to be selected and lead to customer page
+        WebElement okButtonElement = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.okButtonElement));
+        okButtonElement.click();
+
+        {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .ignoring(StaleElementReferenceException.class)
+                .until((WebDriver d) -> {
+                    d.findElement(By.xpath("//tr[@class='sm1-row-no-error  x-grid-row']/child::td/div[text()='EC94791']")).click();
+                    return true;
+                });
+    }
+
         WebElement okButton2 = wait.until(ExpectedConditions.elementToBeClickable(Elements.okButton2));
         okButton2.click();
 
-        // step 11, select inner ok button - > expected: the customer information to be displayed and click on inner ok button
         WebElement innerOkButton = wait.until(ExpectedConditions.elementToBeClickable(Elements.innerOkButton));
         innerOkButton.click();
 
-        // step 12, click on sell in date - > expected: the calendar pop up to appear
         WebElement sellInDate = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.sellInDate));
         sellInDate.click();
 
-
-        // step 13, click on the start date - > expected: the first date to be selected
         WebElement sellInDateValue = wait.until(ExpectedConditions.elementToBeClickable(Elements.sellInDateValue));
         sellInDateValue.click();
 
-        // step 14, click on the range/end date - > expected: the range of dates to appear
         WebElement sellInStartDate = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.sellInStartDate));
         sellInStartDate.click();
 
-        // step 15, select the ok button - > expected: confirmation of the range of dates
         WebElement okButtonSellIn = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.okButtonSellIn));
         okButtonSellIn.click();
 
@@ -116,31 +108,48 @@ public class WhatIfScenario {
         WebElement okButtonSellOut = wait.until(ExpectedConditions.elementToBeClickable(Elements.okButtonSellOut));
         okButtonSellOut.click();
 
-        // step 16, select the products tab - > expected: the page directs user to products page
+        WebElement generalInfo = wait.until(ExpectedConditions.elementToBeClickable(Elements.generalInfo));
+        generalInfo.click();
+
+        WebElement tprRateIn = wait.until(ExpectedConditions.elementToBeClickable(Elements.tprRateIn));
+        tprRateIn.click();
+
         WebElement productCreate = wait.until(ExpectedConditions.elementToBeClickable(Elements.productCreate));
         productCreate.click();
 
-        // step 17, the user selects the add button to add product - > expected: various products to appear
-        WebElement addButtonCreateProd = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.addButtonCreateProd));
+        WebElement addButtonCreateProd = wait.until(ExpectedConditions.elementToBeClickable(Elements.addButtonCreateProd));
         addButtonCreateProd.click();
 
-        // step 18, the user selects the appropriate customer - > expected: the customer details are present and populated
         WebElement customerAdd = wait.until(ExpectedConditions.visibilityOfElementLocated(Elements.customerAdd));
         customerAdd.click();
 
-        // step 19, the user clicks on the ok button to add customer - > expected: the customer is added and details visible
         WebElement okButtonToAddCust = wait.until(ExpectedConditions.elementToBeClickable(Elements.okButtonToAddCust));
         okButtonToAddCust.click();
 
-        // step 20, the user to select the cockpit - > expected: the cockpit to be displayed with multiple tabs accessible
-        WebElement cockPit = wait.until(ExpectedConditions.elementToBeClickable(Elements.cockPit));
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(Elements.saveButton));
+        saveButton.click();
+
+        {
+            new WebDriverWait(driver, Duration.ofSeconds(5))
+                    .ignoring(ElementClickInterceptedException.class)
+                    .until((WebDriver d) -> {
+                        d.findElement(Elements.promoWhatIf).click();
+                        return true;
+                    });
+        }
+
+        WebElement promoOkButton = wait.until(ExpectedConditions.elementToBeClickable(Elements.promoOkButton));
+        promoOkButton.click();
+
+        WebElement cockPit = wait.until(ExpectedConditions.presenceOfElementLocated(Elements.cockPit));
         cockPit.click();
+
     }
 
-//    @After
-//    public void close(){
-//        driver.quit();
-//    }
+    @After
+    public void close() {
+        driver.quit();
+    }
 }
 
 
